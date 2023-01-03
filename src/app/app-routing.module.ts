7386a81c6332597d1aaf93e133405154c55c5c13
Artur <helter@wp.pl>
2023-01-03 17:38:34 +0100

    create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './modules/shared/display/display.component';
+import { MainComponent } from './modules/shared/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'display',
+    component: DisplayComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/shared/header/header.component';
+import { FooterComponent } from './modules/shared/footer/footer.component';
+import { MainComponent } from './modules/shared/main/main.component';
+import { DisplayComponent } from './modules/shared/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
